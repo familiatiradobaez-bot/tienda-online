653345f9e818a82bf044bec304ee8df91f739025
jf-empresa <jf.tirado.empresa@gmail.com>
2026-09-23 03:23:42 -0400

    Fix worker to not import build directly

--- workers/app.ts
+++ workers/app.ts
@@ -1,11 +1,13 @@
 import { createRequestHandler } from "@react-router/cloudflare";
-// @ts-ignore
-import * as build from "../build/server/index.js";
 import type { Env } from "../app/lib/types";
 
 export default {
   async fetch(request: Request, env: Env): Promise<Response> {
-    const handler = createRequestHandler(build, "production");
+    const handler = createRequestHandler(
+      // @ts-ignore - injected by the build
+      typeof __BUILD__ !== "undefined" ? __BUILD__ : {},
+      "production"
+    );
     return handler(request as any, { cloudflare: { env } } as any);
   },
 } satisfies ExportedHandler<Env>;
